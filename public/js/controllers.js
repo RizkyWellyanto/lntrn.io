d64a6010c8cd071401a90449944c88bbe82cc61c
Nihar Sheth <niharrsheth@gmail.com>
2016-12-07 01:39:32 -0600

    minor text fixes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -159,7 +159,7 @@ lntrnioControllers.controller("mainController", ["$scope", "$window", "Posts", "
 		var lant = document.createElement("a");
 		lant.innerHTML = SVG_lantern;
 		lant.style.position = "absolute";
-		lant.style.left = 110 + 'vw';//+100*xperc + 'vw';
+		lant.style.left = 100 + 'vw';//+100*xperc + 'vw';
 		lant.style.top = 100*yperc + 'vh';
 		lant.classList.add("box");
 		lant.setAttribute("ng-click", "$(this).attr(filter, url(#darken));");
